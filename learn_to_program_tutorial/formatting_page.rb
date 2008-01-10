eeec889198e8ac33eee9274e7de92a05e2dea427
reginaldo <reginaldo@c24ce081-ecdb-4f3f-a214-8159a4f5ee6c>
2008-01-10 16:54:15 +0000

    Traduzido.
    git-svn-id: http://opensvn.csie.org/learn_to_program_pt_br/trunk@7 c24ce081-ecdb-4f3f-a214-8159a4f5ee6c

--- learn_to_program_tutorial/formatting_page.rb
+++ learn_to_program_tutorial/formatting_page.rb
@@ -1,23 +1,23 @@
 module FormattingPage
   #
-  #  TEST PAGE FOR FORMATTING
+  #  PÁGINA DE TESTE PARA FORMATAÇÃO
   #
   
   def generateFormattingPage
-    h1 { 'Heading 1' }
-    h2 { 'Heading 2' }
-    h3 { 'Heading 3' }
-    h4 { 'Heading 4' }
+    h1 { 'Cabeçalho 1' }
+    h2 { 'Cabeçalho 2' }
+    h3 { 'Cabeçalho 3' }
+    h4 { 'Cabeçalho 4' }
     
-    para {'Here\'s some code with fake output:'}
-    prog [], '...just kidding, dude...', 'FUNKADELIC!' do <<-'END_CODE'
-      #  Here is some 'Ruby' code.
-      #  5 is better than 6.
-      #  def wilma do end if not in, dude.
+    para {'Aqui está um código com uma saída falsa:'}
+    prog [], '...brincadeirinha, cara...', 'FANTÁSTICO!' do <<-'END_CODE'
+      #  Aqui está um código 'Ruby'.
+      #  5 é melhor do que 6.
+      #  def wilma do end if not in, cara.
       #'
       
-      '#This shouldn\'t cause any problems.'
-      'Neither # should this\\'
+      '#Isto não deveria causar nenhum problema.'
+      'Nem # deveria isto\\'
       'do end if elsif else case when then while def class'
       'or and not next in'
       'to 3 or not to 3, that is 3.7'
@@ -29,7 +29,7 @@ module FormattingPage
       9.9
       5.times {}
       
-      puts 'I love cheese.' # yo 'g'
+      puts 'Eu adoro queijo.' # yo 'g'
       puts 5.02 + 8 + 0.002  # s'up, muva
       
       jimmy = ['yoyoyo', 66]
@@ -37,52 +37,52 @@ module FormattingPage
         puts item.inspect
       end
       
-      puts case 'pumpkin'
-        when String then 'yep'
-        when Fixnum then 'nope'
-        else 'maybe'
+      puts case 'abobora'
+        when String then 'sim'
+        when Fixnum then 'nao'
+        else 'talvez'
       end
       
       def yummy
         if (4 <= 5)
-          'Yummm!'
+          'Uhmmm!'
         elsif (4 == 5)
-          'Huh?'
+          'Ãh?'
         else
           while (1 == 2)
-            puts 'What?'
+            puts 'O quê?'
           end
         end
       end
       
-      class JustSomeClass
+      class ApenasAlgumaClasse
         def initialize
           @var = 5
         end
       end
       
-      puts Math::PI  #  Should work.
-      puts PI        #  Shouldn't work.
+      puts Math::PI  #  Deveria funcionar.
+      puts PI        #  Não deveria funcionar.
       END_CODE
     end
-    para {'Here\'s some code with input and output:'}
+    para {'Aqui está um código com entrada e saída:'}
     prog ['"Chris"', '&26', '<red>'] do <<-END_CODE
       puts 'yoyo...wuddup?'
-      puts 'NAME:'
+      puts 'NOME:'
       name = gets.chomp
-      puts 'AGE:'
+      puts 'IDADE:'
       age = gets.chomp
-      puts 'FAVORITE COLOR'
+      puts 'COR FAVORITA'
       color = gets.chomp
-      puts 'Hello, '+name+', the '+age+' year-old '+color+' lover.'
+      puts 'Olá, '+name+', amante da cor '+color+' de '+age+' anos de idade. '
       END_CODE
     end
     
     para do <<-END_PARAGRAPH
-      Hello there.  I love #{input 'inputting VARIOUS things'}.  I also get a kick
-      out of #{code 'coding various things'}.  There's such a thrill in
-      seeing all of the exciting #{output 'output you can get'} from a
-      well-written program.
+      Olá.  Eu adoro #{input 'colocar VÁRIAS coisas'}.  Eu também fico animado
+      de #{code 'codificar várias coisas'}.  Existe uma tal emoção em
+      ver tudo da excitante #{output 'saída que você pode obter'} de um
+      programa bem escrito.
       END_PARAGRAPH
     end
   end
